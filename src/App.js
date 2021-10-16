94cc2f0738ba1444593e2a4cf96c7bfeb7d5e21d
Valentin <valkjavalkja@gmail.com>
2021-10-16 20:51:26 -0300

    fetching the movieDB api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import Movie from './Components/Movie'
 
+const MAIN_API = 'https://api.themoviedb.org/3/movie/550?api_key=2243846a2a712250e068e5d7ca8ea3ac'
+
 function App() {
 
   const movies = [1,2,3]
+
+  fetch(MAIN_API)
+  .then((response) => response.json())
+  .then((data)=>{
+    console.log(data)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
