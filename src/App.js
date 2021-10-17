4878d6278422b718d27e6163290cc36af56f27f6
Valentin <valkjavalkja@gmail.com>
2021-10-16 21:08:48 -0300

    finally I have the api key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React, {useEffect, UseState} from 'react';
 import './App.css';
 import Movie from './Components/Movie'
 
-const MAIN_API = 'https://api.themoviedb.org/3/movie/550?api_key=2243846a2a712250e068e5d7ca8ea3ac'
+const MAIN_API = 'https://api.themoviedb.org/3/movie/550?api_key=2243846a2a712250e068e5d7ca8ea3ac';
+const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2243846a2a712250e068e5d7ca8ea3ac';
+const IMG_API = 'https_//image.tmdb.org/t/p/w1280';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie/?api_key=2243846a2a712250e068e5d7ca8ea3ac&query='
 
 function App() {
 
